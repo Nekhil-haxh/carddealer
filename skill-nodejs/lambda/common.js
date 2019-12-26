fa8892ceef34e8232ba88126c295109cd5661e56
Nekhil-haxh <53958279+Nekhil-haxh@users.noreply.github.com>
2019-12-26 13:42:47 +0530

    Add files via upload

---
+++ skill-nodejs/lambda/common.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const Alexa = require('ask-sdk-core');
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = "These are the set of available commands, To set number of players, use command Set Players followed by\
+        the number of players, To start distribution, use command distribute,. To get a number of cards, use command give followed by number of cards.\
+        You can start issuing commands,";
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+   const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt("I don't understand this command, try again")
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+// The request interceptor is used for request handling testing and debugging.
+// It will simply log the request in raw json format before any processing is performed.
+const RequestInterceptor = {
+    process(handlerInput) {
+        let { attributesManager, requestEnvelope } = handlerInput;
+        let sessionAttributes = attributesManager.getSessionAttributes();
+
+        // Log the request for debug purposes.
+        console.log(`=====Request==${JSON.stringify(requestEnvelope)}`);
+        console.log(`=========SessionAttributes==${JSON.stringify(sessionAttributes, null, 2)}`);
+    }
+};
+
+module.exports = {
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    IntentReflectorHandler,
+    ErrorHandler,
+    RequestInterceptor
+    };
